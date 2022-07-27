8116454bd718e17ca5c365faee7e375418bd24fd
Даниил Шило <crckhd10@gmail.com>
2022-07-28 00:47:38 +0300

    feat(codewars): add first task from the codewars

---
+++ codewars/Digital Root/solution.ts
@@ -0,0 +1,27 @@
+/**
+ * Calculate digital root of a number
+ * @param n Initial number
+ * @returns Digital root of an parametr number
+ */
+export const digitalRoot = (n: number): number => {
+
+  // Translate our number into string
+  let stringNumber = String(n);
+
+  // While length of the string is not equal 1
+  while (stringNumber.length !== 1) {
+
+    // Total sum of an subnumbers
+    let sum = 0;
+
+    // Splitting number into subnumbers and sum it
+    stringNumber.split("").forEach((num) => {
+      sum += +num;
+    });
+
+    // Return sum into string number variable
+    stringNumber = String(sum);
+  }
+
+  return +stringNumber;
+};
---
+++ codewars/Digital Root/test/digitalRoot.test.ts
@@ -0,0 +1,2 @@
+import { digitalRoot } from "../solution";
+
