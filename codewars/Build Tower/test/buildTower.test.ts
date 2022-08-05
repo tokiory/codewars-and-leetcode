ca974e47d1ba519210b3cd4d1762863d8a17fd1a
Даниил Шило <crckhd10@gmail.com>
2022-08-05 23:02:48 +0300

    feat(codewars): Build Tower

---
+++ codewars/Build Tower/test/buildTower.test.ts
@@ -0,0 +1,28 @@
+import { towerBuilder } from "../solution";
+
+describe("Testing Build Tower Function", () => {
+  test("Build 3 floors", () => {
+    expect(towerBuilder(3)).toEqual(["  *  ", " *** ", "*****"]);
+  });
+
+  test("Build 0 floors", () => {
+    expect(towerBuilder(0)).toEqual([]);
+  });
+
+  test("Build 2 floors", () => {
+    expect(towerBuilder(2)).toEqual([" * ", "***"]);
+  });
+
+  test("Build 1 floor", () => {
+    expect(towerBuilder(1)).toEqual(['*']);
+  });
+
+  test('Testing the length of array', () => {
+    expect(towerBuilder(50).length).toBe(50);
+  });
+
+  test('Testing last floor', () => {
+    const EXPECTED_FLOORS = 30;
+    expect(towerBuilder(EXPECTED_FLOORS)[EXPECTED_FLOORS - 1]).toBe('*'.repeat(EXPECTED_FLOORS * 2 - 1));
+  })
+});
