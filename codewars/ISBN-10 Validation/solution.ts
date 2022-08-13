38a6070fac9fed6a529f2ad0afa6528637016a7b
Даниил Шило <crckhd10@gmail.com>
2022-08-13 13:08:20 +0300

    feat(codewars): ISBN10 Validator

---
+++ codewars/ISBN-10 Validation/solution.ts
@@ -0,0 +1,26 @@
+const ISBN_REGEX = /^[0-9]{9}([0-9]|X)$/gi;
+
+export function validISBN10(isbn: string) {
+  let index = 1;
+  
+  if (isbn.length < 10) {
+    return false;
+  }
+  
+  // https://stackoverflow.com/questions/1520800/why-does-a-regexp-with-global-flag-give-wrong-results
+  // Reinitialize regex index
+  ISBN_REGEX.lastIndex = 0;
+  
+  if (!(ISBN_REGEX.test(isbn))) {
+    return false;
+  }
+  
+  return isbn.split('').reduce((acc, item) => {
+    
+    if (item.toLowerCase() === 'x') {
+      return acc + (10 * index++);
+    }
+    
+    return acc + (Number(item) * index++)
+  }, 0) % 11 === 0;
+}
