454e98257d21f27f5611a3919cc7d8d8603ef110
Даниил Шило <crckhd10@gmail.com>
2022-08-13 12:15:31 +0300

    feat(codewars): Hashtag generator

---
+++ codewars/Hashtag Generator/solution.ts
@@ -0,0 +1,18 @@
+export function generateHashtag (str: string) {
+  
+  if (/^\s*$/.test(str)) {
+    return false;
+  }
+  
+  const words = str.replace(/\s{2,}/, ' ').split(' ').map(word => {
+    return word[0].toUpperCase() + word.slice(1);
+  });
+  
+  const hashtagDescription = words.join('');
+  
+  if (hashtagDescription.length >= 140) {
+    return false;
+  }
+  
+  return '#' + hashtagDescription;
+}
---
+++ codewars/Hashtag Generator/test/hashtagGenerator.test.ts
@@ -0,0 +1,47 @@
+import { generateHashtag } from "../solution";
+
+describe("Testing Hashtag Generator", () => {
+  test("Empty string", () => {
+    expect(generateHashtag("")).toBeFalsy();
+  });
+
+  test("Only spaces string", () => {
+    expect(generateHashtag(" ".repeat(200))).toBeFalsy();
+  });
+
+  test("Basic string", () => {
+    expect(generateHashtag("Do We have A Hashtag")).toBe("#DoWeHaveAHashtag");
+  });
+
+  test("Only word string", () => {
+    expect(generateHashtag("Codewars")).toBe("#Codewars");
+  });
+
+  test("Function need to remove spaces", () => {
+    expect(generateHashtag("Codewars Is Nice")).toBe("#CodewarsIsNice");
+  });
+
+  test("Function must capitilize first letters of words", () => {
+    expect(generateHashtag("Codewars is nice")).toBe("#CodewarsIsNice");
+  });
+
+  test("Long spaces", () => {
+    expect(generateHashtag("code" + " ".repeat(140) + "wars")).toBe(
+      "#CodeWars"
+    );
+  });
+
+  test("Long word", () => {
+    expect(
+      generateHashtag("Looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong Cat")
+    ).toBeFalsy();
+  });
+
+  test("Maximum Length string", () => {
+    expect(generateHashtag("a".repeat(139))).toBe("#" + "A" + "a".repeat(138));
+  });
+
+  test("Overflow of maximum length", () => {
+    expect(generateHashtag("a".repeat(140))).toBeFalsy();
+  });
+});
